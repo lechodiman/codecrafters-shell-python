0b39fc5874b04df2ec6c96fd19299a3c589a4f63
Luis Chodiman <lechodiman@uc.cl>
2024-06-19 17:29:32 -0400

    handle type command

--- app/main.py
+++ app/main.py
@@ -6,17 +6,31 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        # Wait for user input
         input_command = input()
-
         cmd, *args = input_command.split(' ')
+
+        command_types = {
+            "echo": "builtin",
+            "exit": "builtin",
+            "type": "builtin",
+        }
+
+        if cmd not in command_types:
+            sys.stdout.write(f"{input_command}: command not found\n")
+            continue
 
         if cmd == "echo":
             sys.stdout.write(' '.join(args) + '\n')
-        elif input_command == "exit 0":
+        elif cmd == "exit":
             sys.exit(0)
-        else:
-            sys.stdout.write(f"{input_command}: command not found\n")
+        elif cmd == "type":
+            arg = args[0] if args else ""
+            cmd_type = command_types.get(arg, "nonexistent")
+
+            if cmd_type == "builtin":
+                sys.stdout.write(f"{arg} is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{arg}: not found\n")
 
 
 if __name__ == "__main__":
