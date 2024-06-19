22823ea687562b89682f591c6fed9a0ccbe083fb
Luis Chodiman <lechodiman@uc.cl>
2024-06-19 18:05:46 -0400

    Refactor command handling logic for improved extensibility and maintainability

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import os
 import sys
 import subprocess
@@ -12,7 +13,79 @@ def find_executable(cmd: str) -> str:
             return f"{dir}/{cmd}"
 
 
+class Command(ABC):
+    @abstractmethod
+    def execute(self, cmd, *args):
+        pass
+
+
+class EchoCommand(Command):
+    def execute(self, cmd, *args):
+        sys.stdout.write(' '.join(args) + '\n')
+
+
+class ExitCommand(Command):
+    def execute(self, cmd, *args):
+        sys.exit(0)
+
+
+class PwdCommand(Command):
+    def execute(self, cmd, *args):
+        sys.stdout.write(f"{os.getcwd()}\n")
+
+
+class CdCommand(Command):
+    def execute(self, cmd, *args):
+        new_dir = args[0] if args else os.environ.get('HOME')
+        new_dir_exists = os.path.exists(new_dir)
+
+        if new_dir_exists:
+            os.chdir(new_dir)
+        else:
+            sys.stdout.write(f"cd: {new_dir}: No such file or directory\n")
+
+
+class TypeCommand(Command):
+    def __init__(self, builtin_commands):
+        self.builtin_commands = builtin_commands
+
+    def execute(self, cmd, *args):
+        arg = args[0] if args else ""
+
+        if arg in self.builtin_commands:
+            sys.stdout.write(f"{arg} is a shell builtin\n")
+        else:
+            path = find_executable(arg)
+
+            if path:
+                sys.stdout.write(f"{arg} is {path}\n")
+            else:
+                sys.stdout.write(f"{arg}: not found\n")
+
+
+class ExternalCommand(Command):
+    def execute(self, cmd, *args):
+        path = find_executable(cmd)
+
+        if not path:
+            sys.stdout.write(f"{cmd}: command not found\n")
+        else:
+            subprocess.run([cmd] + list(args))
+
+
 def main():
+    builtin_commands = {
+        "echo", "exit", "type", "pwd", "cd"
+    }
+
+    command_types = {
+        "echo": EchoCommand(),
+        "exit": ExitCommand(),
+        "type": TypeCommand(builtin_commands),
+        "pwd": PwdCommand(),
+        "cd": CdCommand()
+    }
+
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -20,49 +93,8 @@ def main():
         input_command = input()
         cmd, *args = input_command.split(' ')
 
-        command_types = {
-            "echo": "builtin",
-            "exit": "builtin",
-            "type": "builtin",
-            "pwd": "builtin",
-            "cd": "builtin"
-        }
-
-        if cmd == "echo":
-            sys.stdout.write(' '.join(args) + '\n')
-        elif cmd == "exit":
-            sys.exit(0)
-        elif cmd == "pwd":
-            sys.stdout.write(f"{os.getcwd()}\n")
-        elif cmd == "cd":
-            new_dir = args[0] if args else os.environ.get('HOME')
-            new_dir_exists = os.path.exists(new_dir)
-
-            if new_dir_exists:
-                os.chdir(new_dir)
-            else:
-                sys.stdout.write(f"cd: {new_dir}: No such file or directory\n")
-
-        elif cmd == "type":
-            arg = args[0] if args else ""
-            cmd_type = command_types.get(arg, "nonexistent")
-
-            if cmd_type == "builtin":
-                sys.stdout.write(f"{arg} is a shell builtin\n")
-            else:
-                path = find_executable(arg)
-
-                if path:
-                    sys.stdout.write(f"{arg} is {path}\n")
-                else:
-                    sys.stdout.write(f"{arg}: not found\n")
-        else:
-            path = find_executable(cmd)
-
-            if not path:
-                sys.stdout.write(f"{input_command}: command not found\n")
-            else:
-                subprocess.run([cmd] + args)
+        command = command_types.get(cmd, ExternalCommand())
+        command.execute(*[cmd] + args)
 
 
 if __name__ == "__main__":
