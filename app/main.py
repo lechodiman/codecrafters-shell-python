65e119d8c1b03441d4e1dff8523ea1a3f13e4ce9
Luis Chodiman <lechodiman@uc.cl>
2024-06-19 17:19:45 -0400

    handle repl

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    user_input = input()
-
-    sys.stdout.write(f"{user_input}: command not found\n")
+        # Wait for user input
+        user_input = input()
+        sys.stdout.write(f"{user_input}: command not found\n")
 
 
 if __name__ == "__main__":
